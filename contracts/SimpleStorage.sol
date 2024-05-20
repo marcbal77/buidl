44bd3b1bf0bbd3de5db5a7f6362799375f28da44
Marc Balestreri <hello@marcbal.com>
2024-05-19 22:36:04 -0700

    public visibilty for simple storage contract regarding fav num variable

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -12,5 +12,14 @@ pragma solidity ^0.8.7; //0.8.12 is another reliable version and there are more
 contract SimpleStorage {
     // This gets initialized to zero!
     // <- this means that this section is a comment
-    uint256 favoriteNumber; 
+    uint256 public favoriteNumber; 
+    // unint256 favorite is the same as uint256 favoriteNumber = 0;
+
+    // functions or methods are a modules that execute some specific set of instructions for us when called
+    function store(uint256 _favoriteNumber) public {
+
+    // for test environment utilize JavaScript VM (London) - a fake local blockchain on remix that allows
+    // a user to simulate transactions really quickly without having to wait to go through a testnet
+        favoriteNumber = _favoriteNumber;
+    }
 }

b3f69e58ce8de7cdb65b30a23c07c31f3517a2e3
Marc Balestreri <hello@marcbal.com>
2024-05-21 21:05:59 -0700

    Add first Struct for SimpleStorage

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -14,6 +14,12 @@ contract SimpleStorage {
     // <- this means that this section is a comment
     uint256 public favoriteNumber; 
     // unint256 favorite is the same as uint256 favoriteNumber = 0;
+    People public person = People({favoriteNumber: 2, name: "Murphy"});
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
 
     // functions or methods are a modules that execute some specific set of instructions for us when called
     function store(uint256 _favoriteNumber) public {
@@ -21,10 +27,13 @@ contract SimpleStorage {
     // for test environment utilize JavaScript VM (London) - a fake local blockchain on remix that allows
     // a user to simulate transactions really quickly without having to wait to go through a testnet
         favoriteNumber = _favoriteNumber;
-        uint256 testVar = 5;
     }
-
+    
+    // keywords view and pure notate a function that doesn't have to spend gas to run - when called alone, they do not cost gas
+    // view or pure functions disallow any modification of state + they disallow reading from the blockchain
     function retrieve() public view returns(uint256){
         return favoriteNumber;
     }
+
+
 }
